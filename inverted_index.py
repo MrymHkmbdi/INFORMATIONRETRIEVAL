cac79823e7553aff3ea539dac7320fb08f1d629e
MrymHkmbdi <57521340+MrymHkmbdi@users.noreply.github.com>
2021-12-02 13:34:58 +0330

    Update inverted_index.py

--- inverted_index.py
+++ inverted_index.py
@@ -1,4 +1,4 @@
-def inverted_index(cleaned_data_file, ):
+def inverted_index(cleaned_data_file):
   inv_idx_dict = {}
   for i in range(0, len(clened_data_file)):
     for word in cleaned_data_file.loc[i, "cleaned_data"].split():
