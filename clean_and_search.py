eeeca0e7af747aa5b00eeeff3aa2c0c2182f27ca
MrymHkmbdi <57521340+MrymHkmbdi@users.noreply.github.com>
2021-12-02 13:31:28 +0330

    add search function

--- clean_and_search.py
+++ clean_and_search.py
@@ -4,18 +4,22 @@ import pandas as pd
 from cleantext import clean
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-from input_cleaning.py import input_cleaning()
+from dataset_cleaning.py import dataset_cleaning()
+from query_cleaning.py import query_cleaning()
+
 
 # read from plot_summaries.txt and convert it to csv file
 df = pd.read_csv('/put plot_summaries.txt directory address here', delimiter='\t', names=['id', 'overview'])
 data = df.to_csv('plot.csv', index=False)
 
-# iterate over 'overview' column of each row and clean it
-ps = PorterStemmer()
-cleans_data=[]
+# cleans movie plots and create a new csv file contains cleaned data
+input_cleaning(data)
 
- 
- # create new csv file which include 'cleaned_data' column
-data.to_csv('plot_cleaned.csv', index=False)  
+# cleans input term and search for it in 'cleaned_data' column of cleaned_data_file and 
+# returns id movies which contain that term
+cleaned_data_file = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/plot_cleaned.csv',encoding='utf-8')
+query_cleaning(cleaned_data_file, input_term)
+
+
 
 
