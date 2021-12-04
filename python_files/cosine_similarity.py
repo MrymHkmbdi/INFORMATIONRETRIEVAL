f2d12c77a292b518dfd12a9877cb38fdf7e8b752
Maryam Hokmabadi <57521340+MrymHkmbdi@users.noreply.github.com>
2021-12-04 10:51:06 +0330

    Delete python_files directory

--- python_files/cosine_similarity.py
+++
@@ -1,7 +0,0 @@
-def cosine_similarity(cleaned_data_file, cleaned_query):
-  tfidf_score = TfidfVectorizer()
-  movie_tfidf = tfidf_score.fit_transform(cleaned_data_file['cleaned_data'].to_list())
-  query_tfidf = tfidf_score.transform([cleaned_query])
-  similarity = cosine_similarity(query_tfidf, movie_tfidf)
-  for i, row in enumerate(similarity):
-      cleaned_data_file.loc[(-row).argsort()[:10]].to_csv("queryandmovies/{}.csv".format(query),index=False)
